a165d46e6e1f422d403174faa45e433c7bf7aa29
salma atouk <salmatk@gmail.com>
2023-05-15 08:39:41 -0700

    ncha2lh

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,21 +10,21 @@
   */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *s;
+unsigned int i;
+char *s;
 
-	if (size == 0)
-		return (NULL);
+if (size == 0)
+return (NULL);
 
-	s = malloc(size * sizeof(char));
+s = malloc(size * sizeof(char));
 
-	if (s == NULL)
-		return (NULL);
+if (s == NULL)
+return (NULL);
 
-	for (i = 0; i < size; i++)
-	{
-		s[i] = c;
-	}
+for (i = 0; i < size; i++)
+{
+s[i] = c;
+}
 
-	return (s);
+return (s);
 }
